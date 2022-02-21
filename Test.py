b7f3426453e9545acd0e948a653f1aa3ca7892be
Shounak Shirodkar <54496663+shounakofficial@users.noreply.github.com>
2022-02-21 09:26:45 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,9 +7,7 @@ from Prog1 import Summation
 
 class TestSum(unittest.TestCase):
     def test_list_int(self):
-        """
-        Test case to find minimum in a list
-        """
+  
         data = [1,2,3,4]
         result = Summation(data)
         self.assertEqual(result, 10)
